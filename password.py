37b67de78f21b3f5ed498888c5e7b1eb6a59c644
邱奕玮 <qiuyiwei@3ekeji.cn>
2018-10-15 18:36:22 +0800

    fix

--- password.py
+++ password.py
@@ -5,7 +5,7 @@ from itertools import permutations
 key = 'L30275849F'
 ps = permutations(key)
 for p in ps:
-    print(p)
+    print(''.join(p))
 
 f_password = 'L379F48502'
 s_password = '02L3F95847'
