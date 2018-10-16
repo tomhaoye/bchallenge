057d04b368df7e3ab8b38abb7d86b2dd452540a4
邱奕玮 <qiuyiwei@3ekeji.cn>
2018-10-16 15:31:52 +0800

    try

--- password.py
+++ password.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 
+import os
 from itertools import permutations
 
 key = 'L30275849F'
 ps = permutations(key)
 for p in ps:
-    print(''.join(p))
+    try_p = ''.join(p)
+    os.system(
+        'openssl enc -aes-256-cbc -md md5 -d -in first_encrypt ' + ' -pass pass:' + try_p + ' >> big_out_file'
+    )
 
 f_password = 'L379F48502'
 s_password = '02L3F95847'
