e95ea8259fba77691cc7172ebafb0816eb0caca2
邱奕玮 <qiuyiwei@3ekeji.cn>
2018-10-17 17:39:11 +0800

    next

--- challenge.py
+++ challenge.py
@@ -6,12 +6,13 @@ img = Image.open('challenge.png')
 content = img.load()
 y = 310
 size = img.size[0]
+y_size = img.size[1]
 
 g_string = []
 for i in range(size):
-    g_string.append(str(bin(content[i, y][0])[-1]))
+    # g_string.append(str(bin(content[i, y][0])[-1]))
+    g_string.append('1' if content[i, y][3] == 254 or content[i, y][3] == 255 else '0')
 g_channel = ''.join(g_string)
 img.close()
-
 
 print hex(int(g_channel, 2))[2:-1].decode('hex')
--- password.py
+++ password.py
@@ -8,7 +8,7 @@ ps = permutations(key)
 for p in ps:
     try_p = ''.join(p)
     os.system(
-        'openssl enc -aes-256-cbc -md md5 -d -in first_encrypt ' + ' -pass pass:' + try_p + ' >> big_out_file'
+        'openssl enc -aes-256-cbc -md md5 -d -in three_encrypt ' + ' -pass pass:' + try_p + ' >> big_out_file'
     )
 
 f_password = 'L379F48502'
